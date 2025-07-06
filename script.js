190c272410a50a3c1f9784cdfd8ded04d9ee88d9
saleesaline <5416553@alunouninter.com>
2025-07-06 15:36:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem(){
+    const divMensagem = document.getElementById('mensagem');
+    divMensagem.innerHTML = "Seja bem vindo à minha página!";
+    divMensagem.style.display="block";
+
+}
